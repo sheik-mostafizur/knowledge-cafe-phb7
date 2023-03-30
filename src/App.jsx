366e0859f20982951572c37a81af85ee8bea0e50
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-03-30 23:01:26 +0600

    setup react files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl text-red-500 font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
