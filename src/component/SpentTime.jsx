6a24af48721d616ce4b34a332aa279f6e865d374
sheik-mostafizur <sheikmostafizur2001@gmail.com>
2023-03-31 03:38:17 +0600

    add spantTime and bookMark

---
+++ src/component/SpentTime.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SpentTime = () => {
+  return (
+    <div className="border p-6 bg-blue-50 border-blue-700 rounded">
+      <h2 className="font-bold text-2xl text-blue-800">
+        Spent time on read : 000 min
+      </h2>
+    </div>
+  );
+};
+
+export default SpentTime;
